076b016b41a136a2074f6518309363b346a4c088
stefangeorgebadescu <stefan.badescu@softvision.com>
2021-06-14 17:14:23 +0300

    da este u

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,69 +2,22 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
 
-
-
-        public  static void main(String[] args) {
-            int number = 49;
-            System.out.println("The number is " + number);
-            if ( number ==49) {
-                System.out.println( '3' );
-            } else{
-                System.out.println( 4 );
-            }
-            System.out.println( "bye" );
-        }
-
-
-    /*public static void SumAverageInt(){
-        int nr;
-        int total = 0;
-
-        for (nr = 1; nr <= 100; nr++) {
-            total = total + nr;
-        }
-        System.out.println(total);
-
-        int initialSum = 0;
-        double averageSum;
-        for (int number = 1; number <= nr-1; ++number) {
-            initialSum += number;
-        }
-        averageSum= initialSum/nr-1;
-        System.out.println("The sum = "+initialSum);
-        System.out.println("The average = "+ averageSum);
-
+    public static void main(String[] args){
+        int number = 56;
+        CheckOddEven(number);
     }
-
-
-    public class CheckPerfectSquare
-    {
-
-        public boolean checkPerfectSquare(double number)
-        {
-
-            double sqrt=Math.sqrt(number);
-
-            return ((sqrt - Math.floor(sqrt)) == 0);
+public  static void CheckOddEven(int number) {   // Save as "CheckOddEven.java"
+              // Set the value of "number" here!
+        System.out.println("The number is " + number);
+        if ( number % 2 ==0) {
+            System.out.println( "EvenNumber" );   // even number
+        } else {
+            System.out.println("OddNumber" );   // odd number
         }
-
-        public void main(String[] args)
-        {
-            System.out.print("6");
-//object of the Scanner class
-            Scanner sc=new Scanner(System.in);
-//reading a number of type double from the user
-            double number=sc.nextDouble();
-//calling the user defined method
-            if (checkPerfectSquare(number))
-                System.out.print("Yes, the given number is perfect square.");
-            else
-                System.out.print("No, the given number is not perfect square.");
-        }
-    }*/
+        System.out.println( "bye" );
+    }
 
 
 }
